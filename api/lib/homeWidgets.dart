f56cb65c084cedf1d9db7474d99afe0bb65c42b3
dharan-dharani <sdharandharani@gamil.com>
2024-10-16 21:56:33 +0530

    modify

---
+++ api/lib/homeWidgets.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HomeWidgets extends StatefulWidget{
+  const HomeWidgets({super.key});
+  @override
+  Home createState()=> Home();
+}
+class Home extends State<HomeWidgets>
+{
+  @override
+  Widget build(BuildContext context)
+  {
+    return const Scaffold();
+  }
+}
